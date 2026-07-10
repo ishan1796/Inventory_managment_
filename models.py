b4cf114b2d10a57d614f4a2275779301e5d4fb57
ishan1796 <ishnsngh2418@gmail.com>
2026-07-10 10:48:12 +0530

    Change User password_hash column to Text for PostgreSQL compatibility

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ db = SQLAlchemy()
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
-    password_hash = db.Column(db.String(150), nullable=False)
+    password_hash = db.Column(db.Text, nullable=False)
     role = db.Column(db.String(50), nullable=False) # 'owner' or 'staff'
 
 class Item(db.Model):
